7498f808c3859254c469869dc69749bc42eb7eff
CristianLegion <108194989+CristianSombra@users.noreply.github.com>
2024-07-01 23:15:27 -0300

    Swiper styles Updated.

--- src/components/swiperProjects/Swipers/SwiperDesktop/SwiperDesktop.tsx
+++ src/components/swiperProjects/Swipers/SwiperDesktop/SwiperDesktop.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper/core'
 import { EffectCoverflow, Pagination } from 'swiper/modules';
@@ -15,6 +16,13 @@ import '@/styles';
 SwiperCore.use([EffectCoverflow, Pagination]);
 
 const SwiperDesktop: React.FC = React.memo(() => {
+
+    const [activeIndex, setActiveIndex] = useState<number>(0);
+
+    const handleSlideChange = (swiper: any) => {
+        setActiveIndex(swiper.realIndex);
+    };
+
     return (
         <div className="container swiper-container">
             <div className="row">
@@ -33,26 +41,33 @@ const SwiperDesktop: React.FC = React.memo(() => {
                         }}
                         pagination={{ clickable: true }}
                         className="mySwiper"
+                        onSlideChange={handleSlideChange}
                     >
                         <SwiperSlide>
-                            <div>
+                            <motion.div 
+                                animate={{opacity: activeIndex === 0 ? 1 : 0}}
+                                >
                                 <FontAwesomeIcon icon={faGithub} className="mb-3 icon-swiper-desktop" />
                                 <FontAwesomeIcon icon={faLaptop} className="mb-3 icon-swiper-desktop" />
-                            </div>
+                            </motion.div>
                             <img src={nursingHRRG} className='img-fluid' alt="Departamento Enfermería" />
                         </SwiperSlide>
                         <SwiperSlide>
-                            <div>
+                            <motion.div
+                                animate={{opacity: activeIndex === 1 ? 1 : 0}}
+                                >
                                 <FontAwesomeIcon icon={faGithub} className="mb-3 icon-swiper-desktop" />
                                 <FontAwesomeIcon icon={faLaptop} className="mb-3 icon-swiper-desktop" />
-                            </div>
+                            </motion.div>
                             <img src={portfolio} className='img-fluid' alt="Prototipo Portafolio" />
                         </SwiperSlide>
                         <SwiperSlide>
-                            <div>
+                            <motion.div
+                                animate={{opacity: activeIndex === 2 ? 1 : 0}}
+                                >
                                 <FontAwesomeIcon icon={faGithub} className="mb-3 icon-swiper-desktop" />
                                 <FontAwesomeIcon icon={faLaptop} className="mb-3 icon-swiper-desktop" />
-                            </div>
+                            </motion.div>
                             <img src={dashboard} className='img-fluid' alt="Prototipo Dashboard" />
                         </SwiperSlide>
                     </Swiper>
@@ -64,69 +79,3 @@ const SwiperDesktop: React.FC = React.memo(() => {
 
 
 export default SwiperDesktop;
-
-
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faLaptop } from '@fortawesome/free-solid-svg-icons/faLaptop';
-// import { faGithub } from '@fortawesome/free-brands-svg-icons';
-
-
-// const SwiperDesktop: React.FC = React.memo(() => {
-//     return (
-//         <div className="container swiper-container">
-//             <div className="row">
-//                 <div className="col-12">
-//                     <Swiper
-//                         effect="coverflow"
-//                         grabCursor={true}
-//                         centeredSlides={true}
-//                         slidesPerView={3}
-//                         coverflowEffect={{
-//                             rotate: 15,
-//                             stretch: 0,
-//                             depth: 300,
-//                             modifier: 1,
-//                             slideShadows: true,
-//                         }}
-//                         modules={[Pagination, EffectCoverflow]}
-//                         className="mySwiper"
-//                     >
-//                         <SwiperSlide>
-//                             <img src={nursingHRRG} className='img-fluid' alt="Departamento Enfermería" />
-//                             {/* <div className="row d-flex justify-content-center">
-//                                 <div className="col-6 col-md-4 col-lg-2 my-3">
-//                                     <FontAwesomeIcon icon={faGithub} className="icon-swiper" />
-//                                 </div>
-//                                 <div className="col-6 col-md-4 col-lg-2 my-3">
-//                                     <FontAwesomeIcon icon={faLaptop} className="icon-swiper" />
-//                                 </div>
-//                             </div> */}
-//                         </SwiperSlide>
-//                         <SwiperSlide>
-//                             <img src={portfolio} className='img-fluid' alt="Prototipo Portafolio" />
-//                             {/* <div className="row d-flex justify-content-center">
-//                                 <div className="col-6 col-md-4 col-lg-2 my-3">
-//                                     <FontAwesomeIcon icon={faGithub} className="icon-swiper" />
-//                                 </div>
-//                                 <div className="col-6 col-md-4 col-lg-2 my-3">
-//                                     <FontAwesomeIcon icon={faLaptop} className="icon-swiper" />
-//                                 </div>
-//                             </div> */}
-//                         </SwiperSlide>
-//                         <SwiperSlide>
-//                             <img src={dashboard} className='img-fluid' alt="Prototipo Dashboard" />
-//                             {/* <div className="row d-flex justify-content-center">
-//                                 <div className="col-6 col-md-4 col-lg-2 my-3">
-//                                     <FontAwesomeIcon icon={faGithub} className="icon-swiper" />
-//                                 </div>
-//                                 <div className="col-6 col-md-4 col-lg-2 my-3">
-//                                     <FontAwesomeIcon icon={faLaptop} className="icon-swiper" />
-//                                 </div>
-//                             </div> */}
-//                         </SwiperSlide>
-//                     </Swiper>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-//});
