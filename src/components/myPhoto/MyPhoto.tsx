d56d4814a97542d6b0166a2d302409e70fe5bc2d
Cristian Sombra <cristiansombra87@gmail.com>
2024-07-10 04:58:43 -0300

    Update performance.

--- src/components/myPhoto/MyPhoto.tsx
+++ src/components/myPhoto/MyPhoto.tsx
@@ -4,11 +4,10 @@ import "@/styles";
 export const MyPhoto: React.FC = React.memo(() => {
     return(
         <div className="mb-5 mb-lg-0 text-center text-md-end" role="img" aria-label="Foto de perfil de mi portafolio">
-            <img 
+            <img
                 src={"https://res.cloudinary.com/nursingstaff/image/upload/v1720391426/Portfolio/NewSection/Home/IMG_7632-Photoroom_v9khon.webp"} 
                 className="img-fluid img-custom"
                 alt="Foto de perfil de mi portafolio"
-                loading="lazy"
             />
         </div>
     );
