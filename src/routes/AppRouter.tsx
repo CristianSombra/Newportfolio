891169f6770d7508518099b47abe5dbe13451db3
Cristian Sombra <cristiansombra87@gmail.com>
2024-07-10 04:30:56 -0300

    Update AppRouter.tsx

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useInView } from 'react-intersection-observer';
 import { Navbar, Footer } from '../layouts';
 import { ToastContainer } from "react-toastify";
 import { ScrollTop } from '../components';
-import { usePageVisibility } from "../hooks";
 import {
     Home,
     About,
@@ -13,25 +13,46 @@ import {
 
 
 const AppRouter: React.FC = () => {
-    const { refs, inView, isNavbarVisible } = usePageVisibility();
+    const [homeRef, homeInView] = useInView({ threshold: 0.40 });
+    const [aboutRef, aboutInView] = useInView({ threshold: 0.20 });
+    const [trajectoryRef, trajectoryInView] = useInView({ threshold: 0.20 });
+    const [experienceRef, experienceInView] = useInView({ threshold: 0.20 });
+    const [contactRef, contactInView] = useInView({ threshold: 0.20 });
+    const [isNavbarVisible, setIsNavbarVisible] = useState(false);
+    const [isDesktop, setIsDesktop] = useState(window.innerWidth <= 992);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsDesktop(window.innerWidth <= 992);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    useEffect(() => {
+        setIsNavbarVisible(isDesktop || aboutInView || trajectoryInView || experienceInView || contactInView);
+    }, [isDesktop, aboutInView, trajectoryInView, experienceInView, contactInView]);
+    
 
     return(
         <>
         {isNavbarVisible && <Navbar />}
-            <div ref={refs.homeRef}>
-                <Home animate={inView.homeInView} />
+            <div ref={homeRef}>
+                <Home animate={homeInView} />
             </div>
-            <div ref={refs.aboutRef}>
-                <About animate={inView.aboutInView} />
+            <div ref={aboutRef}>
+                <About animate={aboutInView} />
             </div>
-            <div ref={refs.trajectoryRef}>
-                <Trajectory animate={inView.trajectoryInView} />
+            <div ref={trajectoryRef}>
+                <Trajectory animate={trajectoryInView} />
             </div>
-            <div ref={refs.experienceRef}>
-                <Experience animate={inView.experienceInView} />
+            <div ref={experienceRef}>
+                <Experience animate={experienceInView} />
             </div>
-            <div ref={refs.contactRef}>
-                <Contact animate={inView.contactInView} />
+            <div ref={contactRef}>
+                <Contact animate={contactInView} />
             </div>
             <Footer />
             <ScrollTop />
