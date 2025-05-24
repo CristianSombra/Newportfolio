ca9cf0ce319a42d78429987594ebb9995e792baf
Cristian Sombra <cristiansombra87@gmail.com>
2025-05-23 21:16:53 -0300

    Corrección posición botón ver más

--- src/components/myPhoto/MyPhoto.tsx
+++ src/components/myPhoto/MyPhoto.tsx
@@ -4,7 +4,7 @@ import "@/styles";
 export const MyPhoto: React.FC = React.memo(() => {
     return(
         <div 
-            className="my-1 mb-5 mt-lg-0 mb-lg-5 text-center text-md-end ms-lg-4" 
+            className="my-1 mb-5 mt-lg-0 mb-lg-0 text-center text-md-end ms-lg-4" 
             role="img" 
             aria-label="Foto de perfil de mi portafolio"
             >
